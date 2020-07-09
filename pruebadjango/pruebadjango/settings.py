e83b96ae8ada907accb72c97d6e74364e36d970a
Abner <abnithos@gmail.com>
2020-07-09 03:56:35 -0500

    Initial commit

--- pruebadjango/pruebadjango/settings.py
+++ pruebadjango/pruebadjango/settings.py
@@ -125,5 +125,5 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER="abnithos@gmail.com"
-EMAIL_HOST_PASSWORD="KORGskti1994%c"
+EMAIL_HOST_USER="pruebadjangoabn@gmail.com"
+EMAIL_HOST_PASSWORD="PRIVADA1994%e"
